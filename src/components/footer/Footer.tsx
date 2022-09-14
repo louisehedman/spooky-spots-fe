38a7d4ba6cb2e71dfeb64ffe908e8a34d0362ab0
louisehedman <louise.hedman@chasacademy.se>
2022-09-14 14:47:29 +0200

    create footer and basic structure about page

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,163 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header: React.FC = () => {
+const Footer: React.FC = () => {
     return (
-        <></>
+        <>
+        <footer className="bg-dark text-white pt-5 pb-4 bg-opacity-75">
+        <div className="container text-center text-md-left">
+          <div className="row text-center text-md-left">
+            <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+                Spooky Spots
+              </h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Vestibulum in aliquet mauris. Nullam auctor dignissim gravida.
+                Mauris vel enim nec lacus elementum iaculis.
+              </p>
+            </div>
+
+            <div className="col-md-2 col-lg-2 col-lx-2 mx-auto mt-3">
+              <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+                About
+              </h5>
+              <p>
+                <Link
+                  to="/about"
+                  className="text-uppercase text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  About us
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/about"
+                  className="text-uppercase text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Contact
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/about"
+                  className="text-uppercase text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Work with us
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="about"
+                  className="text-uppercase text-white"
+                  style={{ textDecoration: "none" }}
+                >
+                  Privacy Policy
+                </Link>
+              </p>
+            </div>
+
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+                Contact
+              </h5>
+              <address>
+                <p>
+                  <i className="fas fa-home mr-3"></i> Spooky Spots, 123 45, SE
+                </p>
+                <p>
+                  <i className="fas fa-envelope mr-3"></i>{" "}
+                  <a
+                    className="text-white text-decoration-none"
+                    href="mailto:spookyspots@gmail.com"
+                  >
+                    spookyspots@gmail.com
+                  </a>
+                </p>
+                <p>
+                  <i className="fas fa-phone mr-3"></i>{" "}
+                  <a
+                    className="text-white text-decoration-none"
+                    href="tel:+4670707070"
+                  >
+                    +46 70 70 70 70
+                  </a>
+                </p>
+              </address>
+            </div>
+          </div>
+
+          <hr className="mb-4" />
+
+          <div className="row align-items-center">
+            <div className="col-md-7 col-lg-8">
+              <p>
+                Copyright ©2022 All rights reserved by:
+                <a href="#" style={{ textDecoration: "none" }}>
+                  <strong className="text-warning"> Spooky Spots</strong>
+                </a>
+              </p>
+            </div>
+
+            <div className="col-md-5 col-lg-4">
+              <div className="text-center text-md-right">
+                <ul className="list-unstyled list-inline">
+                  <li className="list-inline-item">
+                    <a
+                      href="#"
+                      className="btn-floating btn-sm text-white"
+                      style={{ fontSize: "23px" }}
+                    >
+                      <i className="fab fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href="#"
+                      className="btn-floating btn-sm text-white"
+                      style={{ fontSize: "23px" }}
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href="#"
+                      className="btn-floating btn-sm text-white"
+                      style={{ fontSize: "23px" }}
+                    >
+                      <i className="fab fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href="#"
+                      className="btn-floating btn-sm text-white"
+                      style={{ fontSize: "23px" }}
+                    >
+                      <i className="fab fa-linkedin-in"></i>
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href="#"
+                      className="btn-floating btn-sm text-white"
+                      style={{ fontSize: "23px" }}
+                    >
+                      <i className="fab fa-youtube"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+        </>
     )
 }
 
-export default Header;
+export default Footer;
--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from "react";
 
 const About: React.FC = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <>
+      <div className="container my-3">
+        <h2 className="text-center my-4">About Spooky Spots</h2>
+
+        <div className="card rounded mb-4 text-center">
+          <div className="card-body w-75 m-auto">
+            <h3>Our history</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+              minus asperiores. Officiis, cupiditate ut veniam pariatur facilis
+              mollitia odio quaerat molestias similique quis illo quos iure ea
+              sed at nesciunt eum ratione repellat iste recusandae consectetur
+              esse atque soluta! Distinctio deleniti animi veniam repudiandae
+              molestiae cum quis quam repellendus modi.
+            </p>
+
+            <h3>Contact</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Recusandae, explicabo.
+            </p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+
+            <h3>Work with us</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Consequuntur quas suscipit beatae quidem ipsa adipisci voluptatem
+              autem deserunt nihil porro?
+            </p>
+
+            <h3>Privacy policy</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+              dolores animi fugit eos rerum quos rem voluptate porro corrupti
+              aperiam.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default About;
