9e79149dee5bfc81d4f1448eb7446d8709f71165
louisehedman <louise.hedman@chasacademy.se>
2022-09-15 15:11:10 +0200

    add interface IUserDetails

--- src/interfaces/Interfaces.tsx
+++ src/interfaces/Interfaces.tsx
@@ -1,8 +1,14 @@
 export interface IUser {
     _id: string;
-    username: string;
+    username: string | undefined;
     avatar?: string | undefined;
     password: string | undefined;
     email: string | undefined;
     role: number;
+  }
+
+  export interface IUserDetails {
+    username?: string;
+    email?: string;
+    password?: string;
   }
