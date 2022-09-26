6c975a965ee0af022f728cefc4805b4117493d07
louisehedman <louise.hedman@chasacademy.se>
2022-09-26 17:51:32 +0200

    render ghost types, address and rate

--- src/components/pages/spookyspot/SpookySpot.tsx
+++ src/components/pages/spookyspot/SpookySpot.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-import { ISpookySpot } from "../../../interfaces/Interfaces";
 import { API_URL } from "../../../helpers/Urls";
+import { ISpookySpot } from "../../../interfaces/Interfaces";
 
 const SpookySpot: React.FC = () => {
   const [spookySpot, setSpookySpot] = useState<ISpookySpot>();
@@ -14,7 +14,7 @@ const SpookySpot: React.FC = () => {
         setSpookySpot({
           _id: res.data.spookySpot._id,
           name: res.data.spookySpot.name,
-          address: res.data.spookySpot.adress,
+          address: res.data.spookySpot.address,
           postalCode: res.data.spookySpot.postalCode,
           country: res.data.spookySpot.country,
           location: res.data.spookySpot.location,
@@ -33,7 +33,7 @@ const SpookySpot: React.FC = () => {
   return (
     <div>
       <div className="container px-4 pt-4 rounded w-100 mb-4 px-4 pb-4 py-4">
-        <div className="text-center mb-4 py-4">
+        <div className="card text-center mb-4 py-4 shadow-lg">
           <h2 className="text-center py-2">{spookySpot?.name}</h2>
           <img
             className="mx-auto d-block img-fluid"
@@ -46,9 +46,36 @@ const SpookySpot: React.FC = () => {
             }}
           />
         </div>
-        <div className="container px-4 pb-4 py-4 text-left shadow-lg">
+        <div className="card px-4 pb-4 py-4 text-center shadow-lg">
           <h3 className="text-center">Description</h3>
           <p>{spookySpot?.description}</p>
+          <h4 className="border-top py-2">Ghost types</h4>
+          <ul className="list-group w-25 mx-auto border-0">
+            {spookySpot?.ghostTypes.map(({ type }) => (
+              <li className="list-group-item border-0" key={type}>
+                {type}
+              </li>
+            ))}
+          </ul>
+          <h4 className="border-top py-2">Address</h4>
+          <address>
+            <p>{spookySpot?.address}</p>
+            <p>{spookySpot?.postalCode}</p>
+            <p>{spookySpot?.country}</p>
+            <p>
+              Coordinates: {spookySpot?.location.coordinates[0]},{" "}
+              {spookySpot?.location.coordinates[1]}
+            </p>
+            <Link to={"/spookymap"}>
+              <button className="p-1 mx-1 my-3 btn btn-secondary">
+                Go to map
+              </button>
+            </Link>
+          </address>
+          <h4 className="border-top py-2">Spooky rate</h4>
+          <p>
+            <i className="fa-solid fa-ghost" /> {spookySpot?.rating}/5
+          </p>
         </div>
       </div>
     </div>
--- src/interfaces/Interfaces.tsx
+++ src/interfaces/Interfaces.tsx
@@ -1,43 +1,47 @@
 export interface IUser {
-    _id: string;
-    username: string | undefined;
-    avatar?: string | undefined;
-    password: string | undefined;
-    email: string | undefined;
-    role: number;
-  }
+  _id: string;
+  username: string | undefined;
+  avatar?: string | undefined;
+  password: string | undefined;
+  email: string | undefined;
+  role: number;
+}
 
-  export interface IUserDetails {
-    username?: string;
-    email?: string;
-    password?: string;
-  }
+export interface IUserDetails {
+  username?: string;
+  email?: string;
+  password?: string;
+}
 
-  export interface IAuthContext {
-    signedIn: boolean | undefined;
-    handleLogin: (parameter: string) => void;
-    handleLogout: () => void;
-    auth: () => boolean;
-  }
+export interface IAuthContext {
+  signedIn: boolean | undefined;
+  handleLogin: (parameter: string) => void;
+  handleLogout: () => void;
+  auth: () => boolean;
+}
 
-  export interface IGhostType {
-    type: string | undefined;
-    description: string | undefined;
-  }
+export interface IGhostType {
+  type: string | undefined;
+  description: string | undefined;
+}
 
-  export interface ISpookySpot {
-   _id: string | undefined;
-   name: string | undefined;
-   address: string | undefined;
-   postalCode?: string | undefined;
-   country: string | undefined;
-   location: {
-    type: string,
-    coordinates: number[]
-  }
-   description: string | undefined;
-   image?: string | undefined;
-   createdAt: Date;
-   rating: number | undefined;
-   ghostTypes: Array<object> | undefined;
-  }
+export interface ISpookySpot {
+  _id: string | undefined;
+  name: string | undefined;
+  address: string | undefined;
+  postalCode?: string | undefined;
+  country: string | undefined;
+  location: {
+    type: string;
+    coordinates: number[];
+  };
+  description: string | undefined;
+  image?: string | undefined;
+  createdAt: Date;
+  rating: number | undefined;
+  ghostTypes: [
+    {
+      type: string;
+    }
+  ];
+}
