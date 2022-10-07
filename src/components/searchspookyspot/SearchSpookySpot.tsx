df4690d63d7d186007aa4b66e80a6c2134a05fd8
louisehedman <louise.hedman@chasacademy.se>
2022-10-07 15:21:35 +0200

    fetch and set spookySpots

--- src/components/searchspookyspot/SearchSpookySpot.tsx
+++ src/components/searchspookyspot/SearchSpookySpot.tsx
@@ -1,8 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL } from "../../helpers/Urls";
+import { ISpookySpot } from "../../interfaces/Interfaces";
+import "./SearchSpookySpot.css";
+
 const SearchSpookySpot: React.FC = () => {
+    const [spookySpots, setSpookySpots] = useState<ISpookySpot[]>([]);
+    const [search, setSearch]: [string, (search: string) => void] = useState("");
+
+    useEffect(() => {
+        if (spookySpots.length === 0) {
+          try {
+            axios.get(API_URL("spookySpots")).then((response: any) => {
+              setSpookySpots(response.data.spookySpots);
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      });
     
     return (
       <div>
-        
+
       </div>
     );
   };
