5c3f0f8bbb8b44cc30dfe758f42a40f3f7ec87ae
louisehedman <louise.hedman@chasacademy.se>
2022-10-22 17:30:33 +0200

    move serviceWorkerRegistration import to top

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
