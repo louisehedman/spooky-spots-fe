25ed5153d235ae5e4918f77d5e721b9c9f45935a
louisehedman <louise.hedman@chasacademy.se>
2022-10-23 10:04:34 +0200

    update with new eventlistener

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -18,7 +18,6 @@ import {NetworkFirst} from 'workbox-strategies';
 export type {};
 declare const self: ServiceWorkerGlobalScope;
 const cacheName = "::yourserviceworker";
-const version = "v0.0.1";
 
 clientsClaim();
 
@@ -82,107 +81,24 @@ self.addEventListener('message', (event) => {
 });
 
 registerRoute(
-  ({url}) => url.pathname.startsWith('/home'),
+  ({url}) => url.pathname.startsWith('/'),
   new NetworkFirst()
 );
 
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    caches.open(version + cacheName).then(function (cache) {
-      return cache.addAll(["/", "/offline"]);
-    })
-  );
-});
+self.addEventListener('fetch', (event) => {
+  if (event.request.destination === 'image') {
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
 
-self.addEventListener("activate", function (event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      // Remove caches whose name is no longer valid
-      return Promise.all(
-        keys
-          .filter(function (key) {
-            return key.indexOf(version) !== 0;
-          })
-          .map(function (key) {
-            return caches.delete(key);
-          })
-      );
-    })
-  );
-});
+          return networkResponse;
+        });
 
-self.addEventListener("fetch", function (event) {
-  const request = event.request;
-
-  // Always fetch non-GET requests from the network
-  if (request.method !== "GET") {
-    event.respondWith(
-      fetch(request).catch(function () {
-        return caches.match("/offline");
-      }) as Promise<Response>
-    );
-    return;
-  }
-
-  // For HTML requests, try the network first, fall back to the cache,
-  // finally the offline page
-  if (
-    request.headers.get("Accept")?.indexOf("text/html") !== -1 &&
-    request.url.startsWith(this.origin)
-  ) {
-    // The request is text/html, so respond by caching the
-    // item or showing the /offline offline
-    event.respondWith(
-      fetch(request)
-        .then(function (response) {
-          // Stash a copy of this page in the cache
-          const copy = response.clone();
-          caches.open(version + cacheName).then(function (cache) {
-            cache.put(request, copy);
-          });
-          return response;
-        })
-        .catch(function () {
-          return caches.match(request).then(function (response) {
-            // return the cache response or the /offline page.
-            return response || caches.match("/offline");
-          });
-        }) as Promise<Response>
-    );
-    return;
-  }
-
-  // For non-HTML requests, look in the cache first, fall back to the network
-  if (
-    request.headers.get("Accept")?.indexOf("text/plain") === -1 &&
-    request.url.startsWith(this.origin)
-  ) {
-    event.respondWith(
-      caches.match(request).then(function (response) {
-        return (
-          response ||
-          fetch(request)
-            .then(function (response) {
-              const copy = response.clone();
-
-              if (
-                copy.headers.get("Content-Type")?.indexOf("text/plain") === -1
-              ) {
-                caches.open(version + cacheName).then(function (cache) {
-                  cache.put(request, copy);
-                });
-              }
-
-              return response;
-            })
-            .catch(function () {
-              // you can return an image placeholder here with
-              if (request.headers.get("Accept")?.indexOf("image") !== -1) {
-              }
-            })
-        );
-      }) as Promise<Response>
-    );
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+  } else {
     return;
   }
 });
